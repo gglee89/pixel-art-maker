ab304232d09468619955746e7f5779c58d9db011
Giwoo Lee <gglee89@gmail.com>
2017-09-04 22:47:41 +0900

    feat: Removed usage of jQuery; Applied only VanillaJS

--- designs.js
+++ designs.js
@@ -1,64 +1,71 @@
-$(function() {
 
-    // Select color input
-    // Select size input
-    let $color = $("#colorPicker");
-    let color = $color.val();
-    let $height = $("#input_height");
-    let $width = $("#input_width");
-    let $table = $("#pixel_canvas");
+// Select color input
+// Select size input
+let colorEl = document.getElementById("colorPicker");
+let sizePickerEl = document.getElementById("sizePicker");
+let color = colorEl.value;
+let heightEl = document.getElementById("input_height");
+let widthEl = document.getElementById("input_width");
+let tableEl = document.getElementById("pixel_canvas");
 
-    // When size is submitted by the user, call makeGrid()
-    $('#sizePicker').submit((e) => {
+// Handle the submit button event
+sizePickerEl.addEventListener('submit', (e) => {
 
-        e.preventDefault();
+    e.preventDefault();
 
-        makeGrid();
+    makeGrid();
 
-    });
+});
 
-    $color.change((e) => {
+// Update the color that is used to paint
+colorEl.addEventListener('change', (e) => {
 
-        color = e.target.value;
+    color = e.target.value;
 
-    });
+});
 
-    function makeGrid() {
+// Render/Draw the grid
+// Add click event listener that will render the paint
+function makeGrid() {
 
-        $table.html("");
+    tableEl.innerHTML = "";
 
-        // Your code goes here!
-        let height = $height.val();
-        let width = $width.val();
-        let tableContent = "<tbody>";
+    // Your code goes here!
+    let height = heightEl.value;
+    let width = widthEl.value;
+    let tableContent = "<tbody>";
 
-        for(let i = 0; i < height; i++) {
+    for(let i = 0; i < height; i++) {
 
-            tableContent += "<tr>";
+        tableContent += "<tr>";
 
-            for (let j = 0; j < width; j++) {
+        for (let j = 0; j < width; j++) {
 
-                tableContent += "<td class=\"grid\">";
-                tableContent += "</td>";
-
-            }
-
-            tableContent += "</tr>";
+            tableContent += "<td class=\"grid\">";
+            tableContent += "</td>";
 
         }
 
-        tableContent += "</tbody>";
+        tableContent += "</tr>";
 
-        $table.html(tableContent);
+    }
 
-        $(".grid").click((e) => {
+    tableContent += "</tbody>";
+
+    tableEl.innerHTML = tableContent;
+
+    let grid_list = document.getElementsByClassName('grid');
+
+    for (let i = 0; i < grid_list.length; i++) {
+
+        grid_list[i].addEventListener('click', (e) => {
 
             e.preventDefault();
 
-            $(e.target).css({ "background-color": color });
+            e.target.setAttribute("style", "background-color: " + color);
 
         });
 
     }
 
-});
+}
